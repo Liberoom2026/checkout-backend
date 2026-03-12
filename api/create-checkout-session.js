26d3b2d757b947db5fe4b3778029a17597702880
Liberoom2026 <liberoom2026@gmail.com>
2026-03-12 15:11:44 -0300

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 
 export default async function handler(req, res) {
 
-  res.setHeader("Access-Control-Allow-Origin", "https://liberoom.com.br");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
